7751f0eb6eef101172403da0e29310033a98126b
dev-owdenmag <mag.owden@gmail.com>
2025-03-07 11:27:07 +0000

    Updated port

--- app.py
+++ app.py
@@ -188,4 +188,5 @@ def admin_dashboard():
     return render_template('admin_dashboard.html', name_of_admin=admin_name, records=records)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
