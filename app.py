6c46fc4e314759fbdd9b504450903e0218f15688
dev-owdenmag <mag.owden@gmail.com>
2025-03-16 00:18:27 +0000

    Updated Port 2

--- app.py
+++ app.py
@@ -189,5 +189,6 @@ def admin_logout():
     return redirect('/admin/login')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
     
